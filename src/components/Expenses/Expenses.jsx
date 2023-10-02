9f2467362e2549345c68857545fe28e76985fdb3
Khash <khashayar.m.003@gmail.com>
2023-10-02 10:50:18 +0330

    Expense Tracker finished

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
+ 
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
+
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -16,17 +19,7 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No Expenses Found</p>;
 
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key = {expense.id}
-        title = {expense.title}
-        amount = {expense.amount}
-        date = {expense.date}
-      />))
-  }
 
   return (
     <div>
@@ -35,8 +28,8 @@ const Expenses = (props) => {
         selected={filteredYear} 
         onChangeFilter={filterChangeHandler} 
         />
-        
-        {expensesContent}
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
 
       </Card>
     </div>
